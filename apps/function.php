4ca46129dab022fb5b86829deb58f1eae0541fd4
Janzen Faidiban <janzenfaidiban@gmail.com>
2022-06-09 20:37:10 +0900

    final day 05

---
+++ apps/function.php
@@ -0,0 +1,48 @@
+<?php 
+
+    // Penomoran Data Table
+    $no = 1;
+
+    // DATA SPEAKERS
+    $speakers = array (
+        array(
+            "Stenly",
+            "Samberi",
+            "stenlysamberi@gmail.com",
+            "+62-123-4567-8911",
+            "Netar, Sentani Timur, Kabupaten Jayapura, Papua."
+        ),
+
+        array(
+            "James",
+            "Sraun",
+            "james@gmail.com",
+            "+62-123-4567-8911",
+            "Manokwari, Papua Barat."
+        ),
+
+        array(
+            "Nick",
+            "Imbiri",
+            "nick@gmail.com",
+            "+62-123-4567-8911",
+            "Abepura, Waena, Heram, Kota Jayapura, Papua."
+        ),
+
+        array(
+            "Samuel",
+            "Bosawer",
+            "samuel@gmail.com",
+            "+62-123-4567-8911",
+            "Yahim, Waena, Heram, Kota Jayapura, Papua."
+        ),
+
+        array(
+            "Benny",
+            "Marani",
+            "beni@gmail.com",
+            "+62-123-4567-8911",
+            "Perumnas 3, Waena, Heram, Kota Jayapura, Papua."
+        ),
+
+      );
